6e5ebe3acd7234dbd5584966769f2c697bad3ae1
unknown <mikefy@live.com>
2018-03-22 23:21:36 +0800

    add pdf

---
+++ novel_about/download_chapters--2.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+import pymysql
+import requests
+from bs4 import BeautifulSoup
+import re
+import os
+from multiprocessing.dummy import Pool as ThreadPool
+import time
+
+
+def put_data_into_database(target, title, sql_id, path):
+    response = requests.request('get', url=target)
+    if response.status_code != 200:
+        print('Got error in getting page of', target)
+        return True
+    soup = BeautifulSoup(response.content, 'html.parser')
+    word = soup.find('div', id='BookText').text.replace('\xa0', ' ').replace('   ', '\n')
+    path = '/home'+path
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    with open(path + sql_id + '.txt', 'w')as f:
+        f.write('\n'+title+'\n')
+        f.write(word)
+
+
+def download_single_novel(content_url, path, ):
+    start_time = time.time()
+    pool = ThreadPool(100)
+    response1 = requests.get(content_url).content
+    soup1 = BeautifulSoup(response1, 'html.parser')
+    main_chapter = BeautifulSoup(str(soup1.find('div', id='main')), 'html.parser')
+    all_chapter = main_chapter.find_all('a')
+    count = 0
+    for chapter in all_chapter:
+        chapter_number = re.search('\d+', str(chapter)).group(0)
+        title = BeautifulSoup(str(chapter), 'html.parser').text
+        url = content_url + chapter_number + '.html'
+        pool.apply_async(put_data_into_database, [url, title, chapter_number, path])
+        # if put_data_into_database(url, title, chapter_number, database, path):
+        #     continue
+    pool.close()
+    pool.join()
+    end_time = time.time()
+    print('Finish,', 'download', count, 'chapters, takes', end_time - start_time, 'seconds')
+
+
+if __name__ == '__main__':
+    url = 'https://m.uuxs.la/book/42/42176/'
+    local = './book/42176/'
+    # download_single_novel(url, local)
+    dir_path = '/home/book/42176/'
+    file_names = os.listdir(dir_path)
+    with open('42176' + '.txt', 'w') as result_file:
+        for item in file_names:
+            with open(dir_path + '/' + item, 'r') as f:
+                chapter_id = item.replace('.txt', '')
+                result_file.write(f.read())
+                result_file.write('\n')
